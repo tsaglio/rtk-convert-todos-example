fbcd60e8832c862b43a49b9dd4fcf2376e4b8125
Thomas Saglio <tsaglio@zerodaylab.com>
2020-02-25 09:22:27 -0500

    Added prepare() callback to automatically increment todos id.

--- src/features/todos/todosSlice.js
+++ src/features/todos/todosSlice.js
@@ -1,12 +1,19 @@
 import { createSlice } from '@reduxjs/toolkit'
 
+let nextTodoId = 0
+
 const todosSlice = createSlice({
     name: 'todos',
     initialState: [],
     reducers: {
-	addTodo(state, action) {
-	    const { id, text } = action.payload
-	    state.push({ id, text, completed: false })
+	addTodo: {
+	    reducer(state, action) {
+		const { id, text } = action.payload
+		state.push({ id, text, completed: false })
+	    },
+	    prepare(text) {
+		return { payload: { id: nextTodoId++, text } }
+	    }
 	},
 	toggleTodo(state, action) {
 	    const todo = state.find(todo => todo.id === action.payload.id)
